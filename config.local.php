46caa62a93d23f47a1f1adf13072ba0f722702c2
Ezzin Houssam <ezzinhoussam@gmail.com>
2022-02-03 00:20:42 +0100

    bug

---
+++ config.local.php
@@ -0,0 +1,10 @@
+<?php
+$CONF['database_type'] = 'mysqli';
+$CONF['database_host'] = 'localhost';
+$CONF['database_name'] = 'postfix';
+$CONF['database_user'] = 'postfix';
+$CONF['database_password'] = 'choose_a_password';
+
+
+$CONF['configured'] = true;
+?>
